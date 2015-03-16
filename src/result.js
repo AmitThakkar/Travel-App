019b3ed8261c46120f562213116947c8d26fc03a
Amit <amit2.kumar@paytm.com>
2015-03-16 13:44:39 +0530

    Sorting DONE.

--- src/result.js
+++ src/result.js
@@ -1,25 +1,39 @@
 /**
  * Created by namita on 13/3/15.
  */
-(function(ng){
-    var airlineApp = ng.module('airlineApp', []);
-    airlineApp.controller('AirCtrl', ["$scope", "AirlineService", function ($scope, AirlineService) {
-        AirlineService.getAirportMap()
-            .success(function (response) {
-                $scope.result = response;
-            })
-            .error(function (status) {
-                console.log(status);
-            });
-        $scope.searchFlights = function(){
-            AirlineService.getFlightList("Del","Mum")
-                .success(function(response){
-                    $scope.flights = response;
-
-                })
-                .error(function(status){
-                    console.log(status);
-                });
+(function (ng) {
+  var airlineApp = ng.module('airlineApp', []);
+  airlineApp.controller('AirlineController', ["AirlineService", function (AirlineService) {
+    var that = this;
+    AirlineService.getAirportMap()
+      .success(function (response) {
+        that.airportMap = response;
+      })
+      .error(function (status) {
+        console.log(status);
+      });
+    that.searchFlights = function () {
+      AirlineService.getFlightList("Del", "Mum")
+        .success(function (response) {
+          that.flights = response;
+        })
+        .error(function (status) {
+          console.log(status);
+        });
+    };
+    that.sortObject = {
+      sortBy: 'price',
+      price: true
+    };
+    that.sortFlights = function (sortBy) {
+      if (that.sortObject[sortBy]) {
+        that.sortObject[sortBy] = false;
+      } else {
+        that.sortObject = {
+          sortBy: sortBy
         };
-    }]);
+        that.sortObject[sortBy] = true;
+      }
+    };
+  }]);
 })(angular);
